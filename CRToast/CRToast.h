80e965d127001d2988a9743ef5c9eff8a02bd052
Brian Gilham <me@briangilham.com>
2016-09-07 21:52:23 -0400

    Fix Xcode 8 issues.

--- CRToast/CRToast.h
+++ CRToast/CRToast.h
@@ -13,5 +13,5 @@ FOUNDATION_EXPORT const unsigned char CRToastVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CRToast/PublicHeader.h>
 
-#import <CRToast/CRToastConfig.h>
-#import <CRToast/CRToastManager.h>
+#import "CRToastConfig.h"
+#import "CRToast/CRToastManager.h"
